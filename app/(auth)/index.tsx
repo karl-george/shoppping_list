7ce8882dab56ef0e00a9081f4984ac7b5d82cfb0
Karl George <karlgeorge87@gmail.com>
2025-03-24 15:44:10 +0100

    Add ShoppingListCard component and update auth page layout
    - Introduced ShoppingListCard component to display shopping list items.
    - Updated the authenticated layout to replace placeholder text with the ShoppingListCard component.

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,10 +1,12 @@
+import ShoppingListCard from '@/components/ShoppingListCard';
 import React from 'react';
 import { Text, View } from 'react-native';
 
 const Page = () => {
   return (
+    // Flatlist of shopping list cards with a itemseperator
     <View>
-      <Text>AUTHED</Text>
+      <ShoppingListCard />
     </View>
   );
 };
---
+++ components/ShoppingListCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const ShoppingListCard = () => {
+  return (
+    <View>
+      <Text className='text-white'>ShoppingListCard</Text>
+    </View>
+  );
+};
+
+export default ShoppingListCard;
