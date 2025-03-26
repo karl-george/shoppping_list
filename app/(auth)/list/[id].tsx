47a6cb940e2726d39d3f0ca1abe9804f5d26bf6f
Karl George <karlgeorge87@gmail.com>
2025-03-26 14:04:41 +0100

    Enhance list page layout with dynamic header and dropdown menu
    - Integrated useLocalSearchParams to retrieve the page ID for dynamic header title.
    - Added Stack.Screen for customized header styling and included DropDownMenuComponent for navigation options.
    - Updated layout structure for improved organization and readability.

--- app/(auth)/list/[id].tsx
+++ app/(auth)/list/[id].tsx
@@ -1,11 +1,26 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import { Stack, useLocalSearchParams } from 'expo-router';
+import DropDownMenuComponent from '@/components/DropDownMenu';
 
 const Page = () => {
+  const { id } = useLocalSearchParams();
   return (
-    <View>
-      <Text>Page</Text>
-    </View>
+    <>
+      <Stack.Screen
+        options={{
+          title: id as string,
+          headerTitleAlign: 'center',
+          headerStyle: { backgroundColor: '#1E1E1E' },
+          headerTitleStyle: { color: 'white' },
+          headerTintColor: 'white',
+          headerRight: () => <DropDownMenuComponent />,
+        }}
+      />
+      <View>
+        <View></View>
+      </View>
+    </>
   );
 };
 
