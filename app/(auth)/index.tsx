6cd3b237ff8422896434176112d432d1a55689eb
Karl George <karlgeorge87@gmail.com>
2025-03-26 11:01:19 +0100

    Implement FlatList for shopping lists
    - Replaced static ShoppingListCard with a FlatList to dynamically render multiple shopping list items.
    - Introduced dummy data for demonstration purposes, including properties like id, name, emoji, and color.
    - Added item separator for improved visual separation between list items.

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,13 +1,36 @@
 import ShoppingListCard from '@/components/ShoppingListCard';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
 
 const Page = () => {
+  const dummyData = [
+    {
+      id: 1,
+      name: 'Big list of food',
+      products: [],
+      emoji: '🍕',
+      color: '#157E1C',
+    },
+    {
+      id: 2,
+      name: 'Small list of food',
+      products: [],
+      emoji: '🥫',
+      color: '#32288D',
+    },
+  ];
+
   return (
-    // Flatlist of shopping list cards with a itemseperator
-    <View>
-      <ShoppingListCard />
-    </View>
+    <FlatList
+      contentContainerClassName='mt-4'
+      data={dummyData}
+      renderItem={({ item }) => <ShoppingListCard {...item} />}
+      keyExtractor={(item) => item.id.toString()}
+      contentContainerStyle={{ padding: 16, gap: 16 }}
+      ItemSeparatorComponent={() => (
+        <View className='h-0.5 bg-textFaded mt-3' />
+      )}
+    />
   );
 };
 
