bfe4313ae56f1ff4dd09ee23187777910078e5bc
Karl George <karlgeorge87@gmail.com>
2025-03-26 11:45:39 +0100

    Refactor ShoppingListCard component to include navigation and touchable functionality
    - Updated ShoppingListCard to wrap the main view in a TouchableOpacity for navigation.
    - Integrated useRouter for routing to the specific shopping list on press.
    - Added a TODO comment for implementing swipeable delete functionality in the future.

--- components/ShoppingListCard.tsx
+++ components/ShoppingListCard.tsx
@@ -1,21 +1,30 @@
 import { ShoppingLists } from '@/types';
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 
 const ShoppingListCard = ({ name, products, emoji, color }: ShoppingLists) => {
+  const router = useRouter();
+
+  //TODO: Add a swipeable on the right side to reveal a trash icon for deleting the list
+
   return (
-    <View className='flex-row items-center gap-3'>
-      <View
-        className='flex items-center justify-center w-12 h-12 rounded-md'
-        style={{ backgroundColor: color }}
-      >
-        <Text className='text-3xl'>{emoji}</Text>
+    <TouchableOpacity onPress={() => router.push(`/list/${name}`)}>
+      <View className='flex-row items-center gap-3'>
+        <View
+          className='flex items-center justify-center w-12 h-12 rounded-md'
+          style={{ backgroundColor: color }}
+        >
+          <Text className='text-3xl'>{emoji}</Text>
+        </View>
+        <View>
+          <Text className='text-lg text-white'>{name}</Text>
+          <Text className='text-sm text-textFaded'>
+            {products.length} Items
+          </Text>
+        </View>
       </View>
-      <View>
-        <Text className='text-lg text-white'>{name}</Text>
-        <Text className='text-sm text-textFaded'>{products.length} Items</Text>
-      </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
