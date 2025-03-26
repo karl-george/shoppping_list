018a780346efe5bc25edebdb92cea6e0fa0e280f
Karl George <karlgeorge87@gmail.com>
2025-03-26 11:01:28 +0100

    Refactor ShoppingListCard component to accept props
    - Updated ShoppingListCard to accept name, products, emoji, and color as props.
    - Enhanced layout to display emoji and item count with improved styling.

--- components/ShoppingListCard.tsx
+++ components/ShoppingListCard.tsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native';
+import { ShoppingLists } from '@/types';
 import React from 'react';
+import { Text, View } from 'react-native';
 
-const ShoppingListCard = () => {
+const ShoppingListCard = ({ name, products, emoji, color }: ShoppingLists) => {
   return (
-    <View>
-      <Text className='text-white'>ShoppingListCard</Text>
+    <View className='flex-row items-center gap-3'>
+      <View
+        className='flex items-center justify-center w-12 h-12 rounded-md'
+        style={{ backgroundColor: color }}
+      >
+        <Text className='text-3xl'>{emoji}</Text>
+      </View>
+      <View>
+        <Text className='text-lg text-white'>{name}</Text>
+        <Text className='text-sm text-textFaded'>{products.length} Items</Text>
+      </View>
     </View>
   );
 };
