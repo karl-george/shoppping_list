4fcb9e004699efe7d0dc160d62533bcfb904abb4
Karl George <karlgeorge87@gmail.com>
2025-04-01 11:24:53 +0200

    Add input field and buttons to 'home/new-list' for list creation
    - Introduced a TextInput for entering the list name.
    - Implemented a Confirm button for submitting the new list.
    - Enhanced layout for better user interaction and visual consistency.

--- app/(auth)/home/new-list.tsx
+++ app/(auth)/home/new-list.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Button, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 const Page = () => {
   const router = useRouter();
@@ -15,6 +15,25 @@ const Page = () => {
           <Ionicons name='close' size={28} color={'white'} />
         </TouchableOpacity>
       </View>
+      {/* Text Input */}
+      <View className='p-4 mt-8 rounded-md bg-bg'>
+        <TextInput
+          placeholder='List Name'
+          placeholderTextColor={'#716F6F'}
+          className='text-white'
+        />
+      </View>
+
+      {/* Icon and Colour picker */}
+      <View className='flex-row mt-6'>
+        <View className='w-8 h-8 bg-red-600'></View>
+        <View className='w-8 h-8 bg-yellow-600'></View>
+      </View>
+
+      {/* Create Button */}
+      <TouchableOpacity className='self-center justify-center w-48 p-4 mt-8 rounded-md bg-accent'>
+        <Text className='text-lg text-center text-white'>Confirm</Text>
+      </TouchableOpacity>
     </View>
   );
 };
