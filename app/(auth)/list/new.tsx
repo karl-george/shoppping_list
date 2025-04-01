3c32429835c30b39a9353b972158ce3d77007e90
Karl George <karlgeorge87@gmail.com>
2025-04-01 12:11:27 +0200

    Add new item input screen with header and confirmation button
    - Implemented a new screen for adding items, featuring a header with a close button.
    - Added a TextInput for item entry and a Confirm button for submission.
    - Enhanced layout for improved user experience and visual appeal.

--- app/(auth)/list/new.tsx
+++ app/(auth)/list/new.tsx
@@ -1,10 +1,33 @@
-import { View, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
 import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 const Page = () => {
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View className='px-4 mt-4'>
+      {/* Header */}
+      <View className='flex-row items-center justify-center'>
+        <Text className='text-xl text-white'>Add New Item</Text>
+        <TouchableOpacity onPress={router.back} className='absolute right-0'>
+          <Ionicons name='close' size={28} color={'white'} />
+        </TouchableOpacity>
+      </View>
+      {/* Text Input */}
+      <View className='p-4 mt-8 rounded-md bg-bg'>
+        <TextInput
+          placeholder='Strawberries...'
+          placeholderTextColor={'#626262'}
+          className='text-white'
+        />
+      </View>
+
+      {/* Create Button */}
+      <TouchableOpacity className='self-center justify-center w-48 p-4 mt-8 rounded-md bg-accent'>
+        <Text className='text-lg text-center text-white'>Confirm</Text>
+      </TouchableOpacity>
     </View>
   );
 };
