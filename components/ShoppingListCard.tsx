8bfeba280559d1d7819bc61795def00f8024b77d
Karl George <karlgeorge87@gmail.com>
2025-04-02 13:07:07 +0200

    Refactor ShoppingListCard component to improve clarity and remove unused props
    - Updated the ShoppingListCard component to replace 'emoji' with 'icon' for better clarity in prop naming.
    - Removed the products length display, simplifying the UI to just show 'Items'.
    - Cleaned up the code by eliminating a TODO comment related to future features.

--- components/ShoppingListCard.tsx
+++ components/ShoppingListCard.tsx
@@ -3,10 +3,8 @@ import { useRouter } from 'expo-router';
 import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 
-const ShoppingListCard = ({ name, products, emoji, color }: ShoppingLists) => {
+const ShoppingListCard = ({ name, icon, color }: ShoppingLists) => {
   const router = useRouter();
-
-  //TODO: Add a swipeable on the right side to reveal a trash icon for deleting the list
 
   return (
     <TouchableOpacity onPress={() => router.push(`/list/${name}`)}>
@@ -15,13 +13,11 @@ const ShoppingListCard = ({ name, products, emoji, color }: ShoppingLists) => {
           className='flex items-center justify-center w-12 h-12 rounded-md'
           style={{ backgroundColor: color }}
         >
-          <Text className='text-3xl'>{emoji}</Text>
+          <Text className='text-3xl'>{icon}</Text>
         </View>
         <View>
           <Text className='text-lg text-white'>{name}</Text>
-          <Text className='text-sm text-textFaded'>
-            {products.length} Items
-          </Text>
+          <Text className='text-sm text-textFaded'>Items</Text>
         </View>
       </View>
     </TouchableOpacity>
