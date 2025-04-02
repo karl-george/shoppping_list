9a763cb3f13919bf51ef7facc9b7bd0353e0f463
Karl George <karlgeorge87@gmail.com>
2025-04-02 10:01:27 +0200

    Add icon selection screen for user customization
    - Introduced a new screen for selecting icons, featuring a FlatList for easy browsing.
    - Implemented state management using react-native-mmkv to store the selected icon.
    - Enhanced layout with a header and close button for improved navigation.

---
+++ app/(auth)/home/icon-select.tsx
@@ -0,0 +1,43 @@
+import { icons } from '@/constants/Icons';
+import { Ionicons } from '@expo/vector-icons';
+import { useRouter } from 'expo-router';
+import React from 'react';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native';
+import { useMMKVString } from 'react-native-mmkv';
+
+const Page = () => {
+  const [selectedIcon, setSelectedIcon] = useMMKVString('selectedIcon');
+  const router = useRouter();
+
+  return (
+    <View className='px-4 mt-4'>
+      <View className='flex-row items-center justify-center'>
+        <Text className='text-xl text-white'>Pick an Icon</Text>
+        <TouchableOpacity onPress={router.back} className='absolute right-0'>
+          <Ionicons name='close' size={28} color={'white'} />
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={icons}
+        numColumns={4}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 18 }}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        className='mt-8'
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => {
+              setSelectedIcon(item);
+              router.back();
+            }}
+          >
+            <Text className='text-[40px]'>{item}</Text>
+          </TouchableOpacity>
+        )}
+        keyExtractor={(item) => item}
+      />
+    </View>
+  );
+};
+
+export default Page;
